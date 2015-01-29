6886bd0a6dfd9e11029243e4d05097e957a30849
Jörn Mankiewicz <joern.mankiewicz@gmx.de>
2015-01-29 16:22:25 +0100

    .

--- src/log.hpp
+++ src/log.hpp
@@ -80,7 +80,7 @@ namespace lughos
   void debugLogImpl(std::string functionName, std::string fileName, long int lineNumber, std::string message, double severity = 1.0)
   {
     //TODO Implement simultanious output to FILE, DB and CERR
-    boost::posix_time::ptime now = boost::date_time::microsec_clock::local_time();
+    boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
     std::stringstream ss;
     ss << "[" << boost::posix_time::to_iso_extended_string(now) <<  "] <" << severity << "> (" << functionName << "@" << fileName << ":" << lineNumber << " - " << message << std::endl;
     std::string logMessage = ss.str();
