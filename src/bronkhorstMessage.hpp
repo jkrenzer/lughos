50b1ce0996a0b14e750c3cbdfecb30d2928f6fb6
Jörn Mankiewicz <joern.mankiewicz@gmx.de>
2015-02-01 18:48:27 +0100

    .

--- src/bronkhorstMessage.hpp
+++ src/bronkhorstMessage.hpp
@@ -211,15 +211,19 @@ namespace lughos
      std::string getValueString() const
      {
 	std::stringstream returnStream;
-	std::cout << "getValueString HEXVALUE: " << this->hexValue;
+	std::cout << "getValueString HEXVALUE: " << this->hexValue << std::endl;
+	long unsigned int value;
+	std::stringstream(this->hexValue) >> std::hex >> value;
+	std::cout << "getValueString VALUE: " << value << std::endl;
 	switch(this->parameterType)
 	{
-	  case ParameterType::Character : char c; std::stringstream(this->hexValue) >> std::hex >> c; returnStream << c; break;
-	  case ParameterType::Float : float f; std::stringstream(this->hexValue) >> std::hex >> f; returnStream << f; break;
-	  case ParameterType::Integer : uint16_t i; std::stringstream(this->hexValue) >> std::hex >> i; returnStream << i; break;
+	  case ParameterType::Character : char c; memcpy(&c,&value,sizeof(c)); returnStream << c; break;
+	  case ParameterType::Float : float f; memcpy(&f,&value,sizeof(f)); returnStream << f; break;
+	  case ParameterType::Integer : uint16_t i; memcpy(&i,&value,sizeof(i)); returnStream << i; break;
 	  case ParameterType::String : return this->charValue; break;
 	  default: return std::string("ERROR");
 	}
+	std::cout << "getValueString STRING: " << value << std::endl;
 	return returnStream.str();
      }
      
