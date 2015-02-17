97d89fc528b93f73bb96c676e41d6cd26a92a3de
Jörn Mankiewicz <joern.mankiewicz@gmx.de>
2015-02-17 19:23:08 +0100

    .

--- src/RFG.cpp
+++ src/RFG.cpp
@@ -520,7 +520,7 @@ bool RFG::readoutChannels()
   boost::cmatch res1;
   try
   {
-    boost::regex exp1("L([\\s\\S]{5})([\\s\\S]{24})");
+    boost::regex exp1("L([\\s\\S\\0]{5})([\\s\\S\\0]{24})");
     boost::regex_search(s.c_str(), res1, exp1);
   }
   catch(...)
