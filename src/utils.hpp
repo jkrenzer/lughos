17a754ca5666a0a66ac4604fe57f5837935b763a
Jörn Mankiewcz <joern.mankiewicz@gmx.de>
2015-10-21 20:14:56 +0200

    .

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,3 +1,25 @@
+/********************************************************************************
+ *   This file is part of LUGHOS.						*
+ *										*
+ *										*
+ *  LUGHOS is free software: you can redistribute it and/or modify		*
+ *  it under the terms of the GNU General Public License as published by	*
+ *  the Free Software Foundation, either version 3 of the License, or		*
+ *  (at your option) any later version.						*
+ *										*
+ *  LUGHOS is distributed in the hope that it will be useful,			*
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of		*
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		*
+ *  GNU General Public License for more details.				*
+ *										*
+ *  We sincerily invite everybody to participate in LUGHO'S development. 	*
+ *  Together we can achieve more.						*
+ *										*
+ *  (c) 2014, 2015 Jörn Mankiewicz (joern.mankiewicz@gmx.de), 			*
+ *                 Irina Brodski (irina@brodski.de)				*
+ *										*
+ ********************************************************************************/
+
 #ifndef UTILS_HPP
 #define UTILS_HPP
 #include <boost/lexical_cast.hpp>
